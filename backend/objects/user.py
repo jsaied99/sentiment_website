2fe34abf57e69d0a6af166ae5356ae5c9bb61835
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2021-11-17 14:58:12 -0600

    Create user class that hold uid and list of text_sentiment objects
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- backend/objects/user.py
+++ backend/objects/user.py
@@ -1,9 +1,18 @@
-# Defines User Data Structure
+from text_sentiment import TextSentiment
+
+
 class User:
 
-    __slots__ = ["user_id", "password"]
+    __slots__ = ["uid", "user_sentiment_list"]
 
-    def __init__(self, user_id, password):
-        self.user_id = user_id
-        self.password = password
+    def __init__(self, uid, sentiment_data):
+
+        user_sentiment_list = []
+        for data in sentiment_data:
+            text_sentiment_obj = TextSentiment(data['body'], data['sentiment_score'])
+            user_sentiment_list.append(text_sentiment_obj)
+
+        self.uid = uid
+        self.user_sentiment_list = user_sentiment_list
+
 
