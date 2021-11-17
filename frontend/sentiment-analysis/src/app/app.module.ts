1f81d81d8d3ea5ba87abd0c380aafc5f0272814f
Payton Marlin <pjmddc@mail.missouri.edu>
2021-11-16 21:15:45 -0600

    Added basic home page and input

--- frontend/sentiment-analysis/src/app/app.module.ts
+++ frontend/sentiment-analysis/src/app/app.module.ts
@@ -6,16 +6,29 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material.module';
+import { HomeComponent } from './home/home.component';
 
+import { Router, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+import {  FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
