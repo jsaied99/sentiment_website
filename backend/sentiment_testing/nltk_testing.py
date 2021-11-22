dee5ad3e91e46067e2c9412c9ea1bb9ee45353ba
Daniel Buffum <drbuffum13@gmail.com>
2021-11-22 16:36:17 -0600

    Deleted sentiment testing folder, not necessary

--- backend/sentiment_testing/nltk_testing.py
+++
@@ -1,6 +0,0 @@
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-sid = SentimentIntensityAnalyzer()
-print(sid.polarity_scores("Good bad good."))
-
